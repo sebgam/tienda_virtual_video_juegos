1a60d22861354fc1dcfd2126f5d1c085e119fa93
sebas <sebastiangamboa11@gmail.com>
2017-01-30 20:15:19 -0500

    sm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">hola putos</a>
+                    <a href="https://laravel.com/docs">gamboa</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
